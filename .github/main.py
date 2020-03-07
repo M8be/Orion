d9536db53c9b8df5b5413958fecee36364d99edb
M8be <61918915+M8be@users.noreply.github.com>
2020-03-08 00:44:58 +0400

    Create main.py

---
+++ .github/main.py
@@ -0,0 +1,9 @@
+#This is the main file of my program
+#This program purpose is to create and manipulate tags on files in offline system like TagSpaces does.
+#But this time in python, without server-side ability.
+
+#What features I want in this program?
+#A file explorer
+#An image viewer
+#A tag manager with sidecar file (.json or anything else easy to use)
+#
